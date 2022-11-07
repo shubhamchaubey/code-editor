8ab650f9d2a11a30b9cebf4bb0e3033cff946ff1
Shubham Chaubey <shubhamchaubey2211@gmail.com>
2022-11-07 22:16:20 +0530

    modified:   src/components/App.js 	new file:   src/components/Editor.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,31 @@
+import React, { useState } from "react";
+import Editor from "./Editor";
 
 function App() {
-  return "Hello World"
+  const [html, setHtml] = useState('')
+  return (
+    <>
+    <div className="panel top-panel">
+      <Editor 
+      language="xml" 
+      displayName="HTML" 
+      value={html} 
+      onChange={setHtml}
+      />
+      <Editor />
+      <Editor />
+    </div>
+    <div className="panel bottom-panel">
+      <iframe
+      title="output"
+      sandbox="allow-scripts"
+      frameBorder="0"
+      width="100%"
+      height="100%"
+      />
+    </div>
+    </>
+  )
 
 }
 
