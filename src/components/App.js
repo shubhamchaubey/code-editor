899a5f1458e6b9c834c5e896f08944e06fbc769f
Shubham Chaubey <shubhamchaubey2211@gmail.com>
2022-11-23 13:55:09 +0530

    Installed Fontawesome icons 	Used local storage to save the code in local storage 	Designed the expand and compress buttons

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Editor from "./Editor";
+import useLocalStorage from "../hooks/useLocalStorage";
 
 function App() {
-  const [html, setHtml] = useState('')
-  const [css, setCss] = useState('')
-  const [js, setJs] = useState('')
+  const [html, setHtml] = useLocalStorage('html','')
+  const [css, setCss] = useLocalStorage('css','')
+  const [js, setJs] = useLocalStorage('js','')
+  const [srcDoc, setSrcDoc] = useState('')
 
-  const srcDoc = `
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setSrcDoc(`
     <html>
       <body>${html}</body>
       <style>${css}</style>
       <script>${js}</script>
     </html>
-  `
+  `)
+    }, 250)
+
+    return() => clearTimeout(timeout)
+  }, [html, css, js])
+ 
 
   return (
     <>
